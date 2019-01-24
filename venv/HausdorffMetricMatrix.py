5956a4ade290c3c965eeb2dd52e557bf7140ce08
imalka <itai257@gmail.com>
2019-01-24 23:55:25 +0200

    added call for hausdorff metric creation (D matrix)
    need to know what exactly is x and y... they need to be a group

--- venv/HausdorffMetricMatrix.py
+++ venv/HausdorffMetricMatrix.py
@@ -1,6 +1,25 @@
 
 
 class HausdorffMetricMatrix:
+    D = []
 
     def __init__(self,Dist):
-        
+
+        for dist_row in range(len(Dist)):
+            D_row = []
+            for dist_col in range(len(dist_row)):
+                D_row.append(self.HD(dist_row, dist_col, Dist))
+            self.D.append(D_row)
+
+    def HD(self, x, y, Dist):
+        min_over_x = self.get_min_over_x(x,y,Dist)
+        median_over_y = self.get_median_over_y(min_over_x,y,Dist)
+
+        min_over_y = self.get_min_over_y(x,y,Dist)
+        median_over_x = self.get_median_over_x(min_over_y,y,Dist)
+
+        return max(median_over_y, median_over_x)
+
+    def min_over_x(self, x, y, Dist):
+        values = [Dist(i, y) for x in Dist]
+
